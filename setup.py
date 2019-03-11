161dd332d2eb1ef930f401ca9775326b8d5b76a0
Bryan Collazo <bcollazo2010@gmail.com>
2019-03-10 21:51:54 -0400

    Minimal project structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='flush',
+    version='0.1',
+    description='Archive PostgreSQL tables to S3',
+    url='http://github.com/bcollazo/flush',
+    author='Bryan Collazo',
+    author_email='bcollazo2010@gmail.com',
+    license='MIT',
+    packages=['flush'],
+    entry_points={
+        'console_scripts': ['flush=flush.main:main'],
+    },
+    zip_safe=False)

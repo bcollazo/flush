32d7b9e3513f2f6c6dfd036a0d16d4064d3580a7
Bryan Collazo <bcollazo2010@gmail.com>
2019-03-10 22:16:24 -0400

    Ask for DSN via argparse

--- flush/main.py
+++ flush/main.py
@@ -1,9 +1,17 @@
+import argparse
+
 import boto3
 
 
 def main():
-    print('Hello')
+    parser = argparse.ArgumentParser(
+        description='Archive PostgreSQL tables to S3')
+    parser.add_argument(
+        'dsn', help='PostgreSQL DSN (e.g. postgresql://localhost:5432/mydb)')
+    args = parser.parse_args()
 
     s3 = boto3.resource('s3')
     for bucket in s3.buckets.all():
         print(bucket.name)
+
+    print(args.dsn)

e704b17f2990c1969b85d63d766aedd0c0f6b266
Bryan Collazo <bcollazo2010@gmail.com>
2019-03-12 00:10:55 -0400

    Version 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='flush',
-    version='0.3',
+    version='1.0',
     description='Archive PostgreSQL tables to S3',
     long_description=long_description,
     long_description_content_type='text/markdown',
